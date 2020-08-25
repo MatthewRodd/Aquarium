eb26a6ca4c6c832ad401132a3e7653666cdff5c3
MatthewRodd <69178452+MatthewRodd@users.noreply.github.com>
2020-08-25 12:53:31 +1000

    Update aquarium.js
    fixed variables
    added functions
    may be syntax error

--- aquarium.js
+++ aquarium.js
@@ -2,23 +2,37 @@ window.onload=function() {
     lengthObj = document.getElementById('txtLength');
     widthObj = document.getElementById('txtWidth');
 	heightObj = document.getElementById('txtHeight');
-    costObj = document.getElementById('tdCos');
+    costObj = document.getElementById('tdCost');
     document.getElementById('btnReset').onclick = resetInputs;
     document.getElementById('btnCalc').onclick = calccost;
 }
+
 function resetInputs() {
     lengthObj.value = '';
-    if(isNaN(length) || isNaN(width)) {
-        alert('Invalid length or width');
-        return;
-    }
-    costObj.innerHTML = length*Math.pow(width,2)/800;
+    widthObj.value = '';
+	heightObj.value = '';
+    costObj.innerHTML = '';
 }
 
-    widthObj.value = '';
-    costObj.innerHTML = '';
+function surfaceArea() {
+	return (length * width * 2) + (length * height * 2) + (width 8 height * 2);
 }
-function calccost() {
+
+function edges() {
+	return (height * 4) + (width * 2) + (length * 2);
+}
+
+function labourCost() {
+	return (60 / 6000);
+}
+
+function variables() {
     var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
-    costObj.innerHTML = '';
+	var height = new Number(heightObj.value);
+    var glassCost = 0.10;
+	var glueCost = 0.10;
+	costObj.innerHTML = '';	
+    return (edges * glueCost) + (surfaceArea * glassCost) + (labourCost * surfaceArea);
+
+}
